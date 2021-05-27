27a0cec60fa19d45f766aa3e95adb55f35b49c4f
evansteelepdx <evan.steele@evansteele.net>
2021-05-26 22:57:30 -0700

    initial user-based filtering

--- force-app/main/default/lwc/apexLogComponent/apexLogComponent.js
+++ force-app/main/default/lwc/apexLogComponent/apexLogComponent.js
@@ -1,8 +1,12 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 import getApexLogList from "@salesforce/apex/apexLogComponentController.getApexLogList";
+import getTraceTargets from "@salesforce/apex/apexLogComponentController.getTraceTargets";
 
 export default class ApexLogComponent extends LightningElement {
-  searchKey = "";
+  @track searchKey = "";
+  @track logUserValue = "";
+  @track logUserOptions = [];
+
   logIds;
   error;
 
@@ -10,15 +14,35 @@ export default class ApexLogComponent extends LightningElement {
     this.searchKey = event.target.value;
   }
 
+  logUserChange(event) {
+    this.logUserValue = event.target.value;
+  }
+
   handleLoad() {
-    getApexLogList({ searchKey: this.searchKey })
+    getApexLogList({ searchTerm: this.logUserValue })
       .then((result) => {
         this.logIds = result;
         this.error = undefined;
+        console.log(this.logIds);
       })
       .catch((error) => {
         this.error = error;
         this.logIds = undefined;
+      });
+  }
+  connectedCallback() {
+    getTraceTargets({})
+      .then((result) => {
+        var tmpLogOptions = [];
+        for (const element of result) {
+          tmpLogOptions.push({ label: element, value: element });
+        }
+        console.log(tmpLogOptions);
+        this.logUserOptions = JSON.parse(JSON.stringify(tmpLogOptions));
+      })
+
+      .catch((error) => {
+        console.error(error);
       });
   }
 }
